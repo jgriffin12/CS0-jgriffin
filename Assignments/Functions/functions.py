6dd610702007d6e7842249522a93edfdcf5ceba4
Janet Griffin <jgriffin4@mavs.coloradomesa.edu>
2024-03-30 13:18:21 -0400

    Corrections Made - Update

--- Assignments/Functions/functions.py
+++ Assignments/Functions/functions.py
@@ -10,67 +10,59 @@ import math
 def add_two(number1, number2):
     sum = number1 + number2
     print(f"The sum of {number1} and {number2} = {sum}")
+    return sum 
 
 # Function named multiply_two that returns product of two numbers (number1, number2)
 def multiply_two(number1, number2):
     product = number1 * number2
     print(f"The product of {number1} * {number2} = {product}")
+    return product
 
 # Function named divide_two that divides number1 by number2 and returns quotient.
 def divide_two(number1, number2):
     quotient = number1 / number2
     print(f"The quotient of {number1} / {number2} = {quotient}")
+    return quotient
 
 # Function named subtract_two that subtracts number2 from number1 and returns the difference.
 def subtract_two(number1, number2):
     difference = number2 - number1
     print(f"The difference of {number2} - {number1} = {difference}")
+    return difference
 
 # Function named remainder that returns remainder of two numbers. - Make sure this is working correctly.
 def remainder(number1, number2):
     modulo = number1 % number2
     print(f"The remainder of {number1} / {number2} = {modulo}")
+    return modulo 
     
 # Funtion named power that takes number1 to the nth(number2) power and returns the value.
 def power(number1, number2):
     nthpower = number1 ** number2
     print(f"{number1} to the {number2}th power = {nthpower}")
+    return nthpower
 
 # Function named root that takes the square root of number1 
 def root(number1):
     squareRoot1 = math.sqrt(number1)
     print(f"The square root of {number1} = {squareRoot1}")
-
+    return squareRoot1
 
 def test():
-    # 5+6 = 11 == True
-    # -5+10 = 5
-    assert 11, add_two(5,6) == True
-    assert 5, add_two(-5,10) == True
-    # -3 * 10 = 30 == True
-    # 5 * 2 = 10 == True
-    assert -30, multiply_two(-3,10) == True
-    assert 10, multiply_two(5,2) == True
-    # 10 / 4 = 2.5 == True 
-    # 20 / 4 = 5 == True
-    assert 2.5, divide_two(10,4) == True
-    assert 10, divide_two(20,5) == True
-    # number2(25) - number1(50) = -25 --- (number1,number2)
-    # 100 - 30 = 70 == False
-    assert -25, subtract_two(50,25) == True
-    assert 70, subtract_two(30,100) == True
-    # 10%2 = 0 == True
-    # 10%3 = 1 == True
-    assert 2, remainder(10,4) == True
-    assert 1, remainder(10,3) == True
-    # 4^8 = 65536 == True 
-    # 6^2 = 36 == True
-    assert 65536, power(4,8) == True
-    assert 36, power(6,2) == True 
-    # sqrt(4) = 2
-    # sqrt(9) = 3
-    assert 2, root(4) == True
-    assert 3, root(9) == True 
+    assert add_two(2,2) == 4
+    assert add_two(-5,10) == 5
+    assert multiply_two(-3,10) == -30
+    assert multiply_two(5,2) == 10
+    assert divide_two(10,4) == 2.5
+    assert divide_two(20,5) == 4
+    assert subtract_two(50,25) == -25
+    assert subtract_two(30,100) == 70
+    assert 2, remainder(10,4) == 2
+    assert 1, remainder(10,3) == 1
+    assert 65536, power(4,8) == 65536
+    assert 36, power(6,2) == 36
+    assert root(4) == 2
+    assert root(9) == 3
     print("All test cases passed.\n")
     
 
